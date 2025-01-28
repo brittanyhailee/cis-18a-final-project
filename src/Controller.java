3448027e1700f83916a702b6c004b7aba2c0f85f
Brittany Chan <brittanyhaileec@gmail.com>
2025-01-27 22:23:54 -0800

    added welcome message to View

--- src/Controller.java
+++ src/Controller.java
@@ -8,6 +8,7 @@ public class Controller {
     }
 
     public void initView() {
+        view.View();
         view.Menu();
     }
 }
--- src/View.java
+++ src/View.java
@@ -1,5 +1,10 @@
 public class View {
     Inventory inv = new Inventory();
+    public void View() {
+        System.out.println("\t\tWelcome to little miss matcha ⠀\n" + //
+                        "⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀:¨ ·.· ¨:\n" + //
+                        "⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ ⠀⠀ `· . 𐙚" + "\n");
+    }
     public void Menu() {
         inv.showInventory();
     }
